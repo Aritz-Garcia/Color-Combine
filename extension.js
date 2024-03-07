a806d6247a5bbd3fd6db2501819fe57957b2f95e
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2024-03-07 20:12:36 +0100

    Extension tipos de colores

--- extension.js
+++ extension.js
@@ -26,9 +26,10 @@ function getWebviewContentCast() {
 					body {
 						display: flex;
 						justify-content: center;
+						min-width: 270px;
 					}
 
-					h2, h3 {
+					h2, h4 {
 						color: #fff;
 					}
 
@@ -51,25 +52,66 @@ function getWebviewContentCast() {
 						margin-bottom: 50px;
 					}
 					
-					.balanceDiv input, .balanceDiv h3 {
+					.balanceDiv input, .balanceDiv h4 {
 						margin-left: 10px;
+					}
+
+					.btnMedio {
+						display: flex;
+					}
+					
+					.botonesColor {
+						margin: 10px;
+						padding: 8px;
+						border-radius: 5px;
+						border: 2px solid black;
+						font-weight: bold;
+						background-color: #FF7F00;
+						cursor: pointer;
+					}
+					
+					.hover {
+						animation: botonesAnim 0.5s;
+						background-color: #007BFF;
+					}
+					
+					@keyframes botonesAnim {
+						0% {
+							background-color: #FF7F00;
+						}
+						100% {
+							background-color: #007BFF;
+						}
+					}
+					
+					.active {
+						background-color: #007BFF;
+					}
+					
+					.textos {
+						width: 150px;
 					}
 				</style>
 			</head>
 			<body>
 				<div class="container">
 					<div class="row">
+						<div class="btnMedio">
+							<button id="btnHex" class="botonesColor">HEX</button>
+							<button id="btnRgb" class="botonesColor">RGB</button>
+							<button id="btnHsl" class="botonesColor">HSL</button>
+						</div>
 						<div class="col-md-12">
 							<h2>Primer color:</h2>
 							<div class="balanceDiv">
-								<input type="color" value="#ffffff" id="colorUno" class="detector" oninput="cambiar()" />
-								<h3 id="colorUnoP"></h3>
+								<input type="color" value="#FF7F00" id="colorUno" class="detector" oninput="cambiar()" />
+								<h4 id="colorUnoP" class="textos"></h4>
 							</div>
 				
 							<h2>Segundo color:</h2>
 							<div class="balanceDiv">
-								<input type="color" value="#000000" id="colorDos" class="detector" oninput="cambiar()" />
-								<h3 id="colorDosP"></h3>
+								<input type="color" value="#007BFF" id="colorDos" class="detector" oninput="cambiar()" />
+								<h4 id="colorDosP" class="textos"></h4>
 							</div>
 				
 							<h2>Balance:</h2>
@@ -81,14 +123,75 @@ function getWebviewContentCast() {
 							<h2>Resultado:</h2>
 							<div class="balanceDiv">
 								<div class="detector" id="resultado"></div>
-								<h3 id="colorResultadoP"></h3>
+								<h4 id="colorResultadoP" class="textos"></h4>
 							</div>
 						</div>
 					</div>
 				</div>
 				<script>
-					window.onload = cambiar();
+					window.onload = inicio;
 
+					function inicio() {
+						let btnHex = document.getElementById("btnHex");
+						let btnRgb = document.getElementById("btnRgb");
+						let btnHsl = document.getElementById("btnHsl");
+					
+						btnHex.classList.add("active");
+					
+						btnHex.addEventListener("mouseenter", function() {
+							if (!btnHex.classList.contains("active")) {
+								btnHex.classList.add("hover");
+							}
+						});
+					
+						btnHex.addEventListener("mouseleave", function() {
+							btnHex.classList.remove("hover");
+						});
+					
+						btnHex.addEventListener("click", function() {
+							btnHex.classList.add("active");
+							btnRgb.classList.remove("active");
+							btnHsl.classList.remove("active");
+							cambiar();
+						});
+					
+						btnRgb.addEventListener("mouseenter", function() {
+							if (!btnRgb.classList.contains("active")) {
+								btnRgb.classList.add("hover");
+							}
+						});
+					
+						btnRgb.addEventListener("mouseleave", function() {
+							btnRgb.classList.remove("hover");
+						});
+					
+						btnRgb.addEventListener("click", function() {
+							btnRgb.classList.add("active");
+							btnHex.classList.remove("active");
+							btnHsl.classList.remove("active");
+							cambiar();
+						});
+					
+						btnHsl.addEventListener("mouseenter", function() {
+							if (!btnHsl.classList.contains("active")) {
+								btnHsl.classList.add("hover");
+							}
+						});
+					
+						btnHsl.addEventListener("mouseleave", function() {
+							btnHsl.classList.remove("hover");
+						});
+					
+						btnHsl.addEventListener("click", function() {
+							btnHsl.classList.add("active");
+							btnHex.classList.remove("active");
+							btnRgb.classList.remove("active");
+							cambiar();
+						});
+						
+						cambiar();
+					}
+					
 					function cambiar() {
 						let resultado = document.getElementById('resultado');
 						let colorUno = document.getElementById('colorUno').value;
@@ -105,6 +208,7 @@ function getWebviewContentCast() {
 					
 						resultado.style.backgroundColor = rgbAHex(nuevoColor);
 						document.getElementById('colorResultadoP').innerHTML = rgbAHex(nuevoColor).toUpperCase();
+						cambiarCodigos();
 					}
 					
 					function hexARgb(hex) {
@@ -130,11 +234,55 @@ function getWebviewContentCast() {
 						return "#" + r + g + b;
 					}
 					
+					function hexAHsl(hex) {
+						let r = 0, g = 0, b = 0;
+						if (hex.length == 4) {
+							r = "0x" + hex[1] + hex[1];
+							g = "0x" + hex[2] + hex[2];
+							b = "0x" + hex[3] + hex[3];
+						} else if (hex.length == 7) {
+							r = "0x" + hex[1] + hex[2];
+							g = "0x" + hex[3] + hex[4];
+							b = "0x" + hex[5] + hex[6];
+						}
+						// Then to HSL
+						r /= 255;
+						g /= 255;
+						b /= 255;
+						let cmin = Math.min(r,g,b),
+							cmax = Math.max(r,g,b),
+							delta = cmax - cmin,
+							h = 0,
+							s = 0,
+							l = 0;
+					
+						if (delta == 0)
+							h = 0;
+						else if (cmax == r)
+							h = ((g - b) / delta) % 6;
+						else if (cmax == g)
+							h = (b - r) / delta + 2;
+						else
+							h = (r - g) / delta + 4;
+					
+						h = Math.round(h * 60);
+					
+						if (h < 0)
+							h += 360;
+					
+						l = (cmax + cmin) / 2;
+						s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+						s = +(s * 100).toFixed(0);
+						l = +(l * 100).toFixed(0);
+					
+						return [h, s, l];
+					}
+					
 					function cambiarBalance(valor) {
 						if (valor.startsWith("0")) {
 							valor = valor.substring(1);
 						}
-
+						
 						if (valor > 100) {
 							valor = 100;
 						} else if (valor < 0 || isNaN(valor) || valor == "") {
@@ -145,6 +293,45 @@ function getWebviewContentCast() {
 						document.getElementById('balanceInput').value = valor;
 					
 						cambiar();
+					}
+					
+					function cambiarCodigos() {
+						let colorUno = document.getElementById('colorUno').value;
+						let colorDos = document.getElementById('colorDos').value;
+						let resultadoRgb = document.getElementById('resultado').style.backgroundColor;
+						let resultadoRgbAHex = resultadoRgb.substring(4, resultadoRgb.length - 1);
+						resultadoRgbAHex = resultadoRgbAHex.split(", ");
+						resultadoRgbAHex = resultadoRgbAHex.map(function (x) {
+							return parseInt(x);
+						});
+						let resultado = rgbAHex(resultadoRgbAHex);
+					
+						if (document.getElementById('btnHex').classList.contains("active")) {
+							// Hex
+							document.getElementById('colorUnoP').innerHTML = colorUno.toUpperCase();
+							document.getElementById('colorDosP').innerHTML = colorDos.toUpperCase();
+							document.getElementById('colorResultadoP').innerHTML = resultado.toUpperCase();
+					
+						} else if (document.getElementById('btnRgb').classList.contains("active")) {
+							// Rgb
+							let rgb1 = hexARgb(colorUno);
+							let rgb2 = hexARgb(colorDos);
+							let rgbResultado = hexARgb(resultado);
+					
+							document.getElementById('colorUnoP').innerHTML = "RGB(" + rgb1[0] + ", " + rgb1[1] + ", " + rgb1[2]+ ")";
+							document.getElementById('colorDosP').innerHTML = "RGB(" + rgb2[0] + ", " + rgb2[1] + ", " + rgb2[2]+ ")";
+							document.getElementById('colorResultadoP').innerHTML = "RGB(" + rgbResultado[0] + ", " + rgbResultado[1] + ", " + rgbResultado[2]+ ")";
+					
+						} else if (document.getElementById('btnHsl').classList.contains("active")) {
+							// Hsl
+							let hsl1 = hexAHsl(colorUno);
+							let hsl2 = hexAHsl(colorDos);
+							let hslResultado = hexAHsl(resultado);
+					
+							document.getElementById('colorUnoP').innerHTML = "HSL(" + hsl1[0] + ", " + hsl1[1] + "%, " + hsl1[2]+ "%)";
+							document.getElementById('colorDosP').innerHTML = "HSL(" + hsl2[0] + ", " + hsl2[1] + "%, " + hsl2[2]+ "%)";
+							document.getElementById('colorResultadoP').innerHTML = "HSL(" + hslResultado[0] + ", " + hslResultado[1] + "%, " + hslResultado[2]+ "%)";
+						}
 					}
 				</script>
 			</body>
--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,65 @@
-window.onload = cambiar();
+window.onload = inicio;
+
+function inicio() {
+    let btnHex = document.getElementById("btnHex");
+    let btnRgb = document.getElementById("btnRgb");
+    let btnHsl = document.getElementById("btnHsl");
+
+    btnHex.classList.add("active");
+
+    btnHex.addEventListener("mouseenter", function() {
+        if (!btnHex.classList.contains("active")) {
+            btnHex.classList.add("hover");
+        }
+    });
+
+    btnHex.addEventListener("mouseleave", function() {
+        btnHex.classList.remove("hover");
+    });
+
+    btnHex.addEventListener("click", function() {
+        btnHex.classList.add("active");
+        btnRgb.classList.remove("active");
+        btnHsl.classList.remove("active");
+        cambiar();
+    });
+
+    btnRgb.addEventListener("mouseenter", function() {
+        if (!btnRgb.classList.contains("active")) {
+            btnRgb.classList.add("hover");
+        }
+    });
+
+    btnRgb.addEventListener("mouseleave", function() {
+        btnRgb.classList.remove("hover");
+    });
+
+    btnRgb.addEventListener("click", function() {
+        btnRgb.classList.add("active");
+        btnHex.classList.remove("active");
+        btnHsl.classList.remove("active");
+        cambiar();
+    });
+
+    btnHsl.addEventListener("mouseenter", function() {
+        if (!btnHsl.classList.contains("active")) {
+            btnHsl.classList.add("hover");
+        }
+    });
+
+    btnHsl.addEventListener("mouseleave", function() {
+        btnHsl.classList.remove("hover");
+    });
+
+    btnHsl.addEventListener("click", function() {
+        btnHsl.classList.add("active");
+        btnHex.classList.remove("active");
+        btnRgb.classList.remove("active");
+        cambiar();
+    });
+    
+    cambiar();
+}
 
 function cambiar() {
     let resultado = document.getElementById('resultado');
@@ -16,6 +77,7 @@ function cambiar() {
 
     resultado.style.backgroundColor = rgbAHex(nuevoColor);
     document.getElementById('colorResultadoP').innerHTML = rgbAHex(nuevoColor).toUpperCase();
+    cambiarCodigos();
 }
 
 function hexARgb(hex) {
@@ -41,6 +103,50 @@ function rgbAHex(rgb) {
     return "#" + r + g + b;
 }
 
+function hexAHsl(hex) {
+    let r = 0, g = 0, b = 0;
+    if (hex.length == 4) {
+        r = "0x" + hex[1] + hex[1];
+        g = "0x" + hex[2] + hex[2];
+        b = "0x" + hex[3] + hex[3];
+    } else if (hex.length == 7) {
+        r = "0x" + hex[1] + hex[2];
+        g = "0x" + hex[3] + hex[4];
+        b = "0x" + hex[5] + hex[6];
+    }
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r,g,b),
+        cmax = Math.max(r,g,b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+
+    if (delta == 0)
+        h = 0;
+    else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+        h = (b - r) / delta + 2;
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    if (h < 0)
+        h += 360;
+
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(0);
+    l = +(l * 100).toFixed(0);
+
+    return [h, s, l];
+}
+
 function cambiarBalance(valor) {
     if (valor.startsWith("0")) {
         valor = valor.substring(1);
@@ -56,4 +162,43 @@ function cambiarBalance(valor) {
     document.getElementById('balanceInput').value = valor;
 
     cambiar();
+}
+
+function cambiarCodigos() {
+    let colorUno = document.getElementById('colorUno').value;
+    let colorDos = document.getElementById('colorDos').value;
+    let resultadoRgb = document.getElementById('resultado').style.backgroundColor;
+    let resultadoRgbAHex = resultadoRgb.substring(4, resultadoRgb.length - 1);
+    resultadoRgbAHex = resultadoRgbAHex.split(", ");
+    resultadoRgbAHex = resultadoRgbAHex.map(function (x) {
+        return parseInt(x);
+    });
+    let resultado = rgbAHex(resultadoRgbAHex);
+
+    if (document.getElementById('btnHex').classList.contains("active")) {
+        // Hex
+        document.getElementById('colorUnoP').innerHTML = colorUno.toUpperCase();
+        document.getElementById('colorDosP').innerHTML = colorDos.toUpperCase();
+        document.getElementById('colorResultadoP').innerHTML = resultado.toUpperCase();
+
+    } else if (document.getElementById('btnRgb').classList.contains("active")) {
+        // Rgb
+        let rgb1 = hexARgb(colorUno);
+        let rgb2 = hexARgb(colorDos);
+        let rgbResultado = hexARgb(resultado);
+
+        document.getElementById('colorUnoP').innerHTML = "RGB(" + rgb1[0] + ", " + rgb1[1] + ", " + rgb1[2]+ ")";
+        document.getElementById('colorDosP').innerHTML = "RGB(" + rgb2[0] + ", " + rgb2[1] + ", " + rgb2[2]+ ")";
+        document.getElementById('colorResultadoP').innerHTML = "RGB(" + rgbResultado[0] + ", " + rgbResultado[1] + ", " + rgbResultado[2]+ ")";
+
+    } else if (document.getElementById('btnHsl').classList.contains("active")) {
+        // Hsl
+        let hsl1 = hexAHsl(colorUno);
+        let hsl2 = hexAHsl(colorDos);
+        let hslResultado = hexAHsl(resultado);
+
+        document.getElementById('colorUnoP').innerHTML = "HSL(" + hsl1[0] + ", " + hsl1[1] + "%, " + hsl1[2]+ "%)";
+        document.getElementById('colorDosP').innerHTML = "HSL(" + hsl2[0] + ", " + hsl2[1] + "%, " + hsl2[2]+ "%)";
+        document.getElementById('colorResultadoP').innerHTML = "HSL(" + hslResultado[0] + ", " + hslResultado[1] + "%, " + hslResultado[2]+ "%)";
+    }
 }
